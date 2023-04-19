ea23069fdee40ebe7a539bc93fd938f79453b6f9
vaishali-knightrider <vaishali-knightrider@github.com>
2023-04-19 13:56:49 +0530

    stad binary search program added

---
+++ binary_search/std_binary_search.cpp
@@ -0,0 +1,16 @@
+/*
+Q: 
+Given a sorted array and a target elem,
+find index of target in ar, if target is
+not present return -1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x;
+	cin>>x;
+
+	cout<<"helo "<<x;
+}
